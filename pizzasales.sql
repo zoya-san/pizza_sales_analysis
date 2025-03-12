57a6277cc2f047dc23dd4e8859bf26af42516e52
zoya-san <Zoyashaikh1699@gmail.com>
2025-03-12 14:39:28 +0530

    Update pizzasales.sql
    Added following queries:
    Total Revenue – Calculate the total revenue generated from pizza sales.
    Average Order Value – Determine the average value of each order.
    Total Pizzas Sold – Identify how many pizzas were sold in total.

--- pizzasales.sql
+++ pizzasales.sql
@@ -1,6 +1,18 @@
 select *  from pizza.pizza_sales;
 
+-- 1. Total Revenue:
 select sum(total_price) as Total_Revenue from pizza.pizza_sales;
+
+-- 2. Average Order Value:
+select sum(total_price)/count(distinct(order_id)) as average_order_value from pizza.pizza_sales;
+
+-- 3. Total Pizzas Sold
+select sum(quantity) as total_pizzas_sold from pizza.pizza_sales;
+
+-- 4. Total Orders
+select sum(distinct(order_id)) as total_orders from pizza.pizza_sales;
+
+
 
 
 
